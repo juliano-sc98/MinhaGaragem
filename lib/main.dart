00ecfddf2dfb7a0ee2f97dbcae6b5ac04bfc5007
Artur Esteves <artur.correaf@gmail.com>
2020-09-16 11:42:01 -0300

    Adicionada página manutenção

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:MinhaGaragem/screens/introduction/introduction_screen.dart';
+import 'package:MinhaGaragem/screens/manutencao/maint_screen.dart';
 import 'package:MinhaGaragem/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Minha Garagem",
-    home: IntroductionScreen(),
-    theme: theme()
-    ),
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Minha Garagem",
+        home: MyApp(),
+        theme: theme()),
   );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen();
+  }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:MinhaGaragem/constants.dart';
+import 'package:MinhaGaragem/screens/manutencao/maint_screen.dart';
 import 'package:MinhaGaragem/screens/user/user_screen.dart';
 import 'package:MinhaGaragem/size_config.dart';
 import 'package:flutter/material.dart';
@@ -18,98 +19,80 @@ class _HomeScreenState extends State<HomeScreen> {
       key: _scaffoldKey,
       appBar: buildAppBar(),
       body: Container(
-      color: kPrimaryLightColor,
-
+        color: kPrimaryLightColor,
         child: Center(
-
           child: Container(
             color: kPrimaryLightColor,
             width: MediaQuery.of(context).size.width,
             height: getProportionateScreenHeight(280),
-
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 GestureDetector(
                   child: Container(
-
                     width: getProportionateScreenWidth(110),
                     height: getProportionateScreenHeight(110),
                     decoration: BoxDecoration(
-
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                        image: AssetImage("assets/images/suv.png"),
-                          fit: BoxFit.cover
-                      ),
+                          image: AssetImage("assets/images/suv.png"),
+                          fit: BoxFit.cover),
                     ),
                   ),
-
                 ),
-
                 Padding(padding: EdgeInsets.only(top: 10.0)),
-                Text("Carro", style: TextStyle(
+                Text(
+                  "Carro",
+                  style: TextStyle(
                     fontSize: getProportionateScreenWidth(18),
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-
                 Padding(padding: EdgeInsets.only(bottom: 10.0)),
-                Text("Seu carro está em dia com as contas", style: TextStyle(
-                  fontSize: getProportionateScreenWidth(14),
-                  color: Colors.black,
+                Text(
+                  "Seu carro está em dia com as contas",
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(14),
+                    color: Colors.black,
                   ),
                 ),
-
               ],
             ),
           ),
         ),
       ),
-
       bottomNavigationBar: Container(
         color: Colors.white,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-
             NavItem(
               icon: "assets/icons/manutencao.svg",
               text: "Manutenção",
               press: () {
-
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => CarDetails()));
               },
             ),
-
             NavItem(
               icon: "assets/icons/carro.svg",
-              text: "Cadatro veículo",
-              press: () {
-
-              },
+              text: "+ Veículo",
+              press: () {},
             ),
-
             NavItem(
               icon: "assets/icons/dolar.svg",
               text: "Taxas",
-              press: () {
-
-              },
+              press: () {},
             ),
-
             NavItem(
               icon: "assets/icons/aviso.svg",
               text: "Multas",
-              press: () {
-
-              },
+              press: () {},
             ),
-
           ],
         ),
       ),
-
     );
   }
 
@@ -117,29 +100,24 @@ class _HomeScreenState extends State<HomeScreen> {
     return AppBar(
       backgroundColor: kPrimaryColor,
       leading: IconButton(
-
         icon: Icon(
           Icons.exit_to_app,
           color: Colors.black,
         ),
-
         onPressed: () {
           showDialog(
               context: context,
               builder: (context) {
-
                 return AlertDialog(
                   title: Text("Sair"),
                   content: Text("Deseja sair dessa página?"),
                   actions: <Widget>[
-
                     FlatButton(
                       child: Text("Cancelar"),
                       onPressed: () {
                         Navigator.pop(context);
                       },
                     ),
-
                     FlatButton(
                       child: Text("Sim"),
                       onPressed: () {
@@ -149,11 +127,9 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ],
                 );
-              }
-          );
+              });
         },
       ),
-
       title: Text(
         "Olá Visitante",
         style: TextStyle(color: Colors.black),
@@ -161,14 +137,12 @@ class _HomeScreenState extends State<HomeScreen> {
       centerTitle: true,
       elevation: 0,
       actions: <Widget>[
-
         IconButton(
             icon: ClipOval(child: Image.asset("assets/images/person.png")),
             onPressed: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute
-                (builder: (context) => UserScreen()));
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => UserScreen()));
             }),
-
       ],
     );
   }
@@ -196,12 +170,10 @@ class NavItem extends StatelessWidget {
       ),
       child: Column(
         children: <Widget>[
-
           SvgPicture.asset(icon, height: 28, color: kPrimaryColor),
-
           Spacer(),
-
-          Text(text,
+          Text(
+            text,
             style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
           )
         ],
